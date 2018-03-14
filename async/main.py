6b9ef1ff5a3bae8f94f1f85a8eb108fc4759d1dc
Unknown <tobias.trinks@code.berlin>
2018-03-14 17:21:15 +0100

    - checked wave duration - updated light interface - fixed speech api requests

--- async/main.py
+++ async/main.py
@@ -1,5 +1,7 @@
 # libraries
+import os
 import requests
+import time
 import json
 import pyaudio
 import RPi.GPIO as GPIO
@@ -15,8 +17,19 @@ from ordering.speech_processing import SpeechProcessing
 import ordering.speech_processing_threads as SpeechProcessingThreads
 from ordering.order_request import OrderRequest
 
-orderRequest = OrderRequest("http://138.68.71.39:3200/order", "http://138.68.71.39:5200/order")
+orderApi = "http://138.68.71.39:3200/order"
+analyzeApi = "http://138.68.71.39:5200/order"
+orderRequest = OrderRequest(orderApi, analyzeApi)
 # orderRequest_getOnly = OrderRequest("http://138.68.71.39:3200/order?getonly=1", False)
+
+
+def check_connection(host):
+    # check connection
+    response = os.system("ping -c 1 " + host)
+    while response != 0:
+        response = os.system("ping -c 1 " + host)
+        lights_change(255, 0, 0, 5, 1)
+        time.sleep(10)
 
 
 def lights_change(r, g, b, intensity=100, duration=0):
@@ -52,60 +65,85 @@ def main():
 
     recording_thread = Record(pyaudio.PyAudio())
 
-    url = "http://138.68.71.39:3000/orderkeywords"
+    url = "http://138.68.71.39:3200/orderkeywords"
     headers = {
         'Content-Type': "application/json",
         'Cache-Control': "no-cache"
     }
     menu_keywords = requests.request("GET", url, headers=headers)
     menu_keywords = json.loads(menu_keywords.text)
+
+    check_connection("138.68.71.39")
+
     while True:
         state = GPIO.input(BUTTON)
 
         if not state and not button:
+            check_connection("138.68.71.39")
             recording_thread.start()
-            lights_change(255, 255, 255)
+            # lights_change(255, 255, 255)
             button = True
 
         if state and button:
             lights = Lights("pulse", {"r": 255, "g": 255, "b": 255})
             lights.start()
 
-            wave_output = recording_thread.stop()
-            recording_thread.join()
-            # reinitialize thread
-            recording_thread = Record(pyaudio.PyAudio())
+            recording_thread.stop()
+            wave_output = recording_thread.join()
 
-            speech_processing = SpeechProcessing(wave_output)
+            if wave_output:
+                # reinitialize thread
+                recording_thread = Record(pyaudio.PyAudio())
+                speech_processing = SpeechProcessing(wave_output)
 
-            google = SpeechProcessingThreads.GoogleSpeech(speech_processing, menu_keywords)
-            google.start()
+                google = SpeechProcessingThreads.GoogleSpeech(speech_processing, menu_keywords)
+                google.start()
 
-            wit = SpeechProcessingThreads.WitAi(speech_processing)
-            wit.start()
+                wit = SpeechProcessingThreads.WitAi(speech_processing)
+                wit.start()
 
-            bing = SpeechProcessingThreads.BingVoice(speech_processing)
-            bing.start()
+                bing = SpeechProcessingThreads.BingVoice(speech_processing)
+                bing.start()
 
-            order = bing.join()
-            print_headline = 'BING'
-            if not order:
-                order = wit.join()
-                print_headline = 'WIT AI'
-                if not order:
-                    print_headline = 'GOOGLE'
-                    order = google.join()
+                result = {
+                    "items": []
+                }
 
-            result = orderRequest.request(order)
+                order = bing.join()
+                if order:
+                    result = orderRequest.request(order)
+                print_headline = 'BING'
+
+                if not len(result["items"]):
+                    order = wit.join()
+                    if order:
+                        result = orderRequest.request(order)
+                    print_headline = 'WIT AI'
+
+                    if not len(result["items"]):
+                        print_headline = 'GOOGLE'
+                        if order:
+                            result = orderRequest.request(order)
+                        order = google.join()
+
+                print(print_headline + ' order: ' + order)
+            else:
+                order = False
+                print_headline = False
 
             lights.stop()
             lights.join()
 
-            if result:
-                lights_change(r=0, g=255, b=0, duration=1)
-                result_output(order, result, '------------- ' + print_headline + ' -------------')
+            if wave_output:
+                if len(result["items"]):
+                    lights_change(r=0, g=255, b=0, duration=1)
+                    result_output(order, result, '------------- ' + print_headline + ' -------------')
+                else:
+                    lights_change(r=255, g=0, b=0, duration=1)
             else:
-                lights_change(r=255, g=0, b=0, duration=1)
+                lights_change(0, 0, 0)
+                # reinitialize thread
+                recording_thread = Record(pyaudio.PyAudio())
 
             button = False
 
--- async/record.py
+++ async/record.py
@@ -1,7 +1,9 @@
-import os
 import pyaudio
 import threading
 import wave
+
+from interface.lights import Lights
+
 Thread = threading.Thread
 
 FORMAT = pyaudio.paInt16
@@ -22,13 +24,18 @@ class Record(Thread):
 
         self.stream = None
         self.frames = []
+        self.tooShort = False
         self.stopped = False
 
     def run(self):
         print('thread started')
         self.stream = self.audio_interface.open(format=FORMAT, channels=CHANNELS,
-                                 rate=RATE, input=True,
-                                 frames_per_buffer=CHUNK)
+                                                rate=RATE, input=True,
+                                                frames_per_buffer=CHUNK)
+        lights = Lights("change", {"r": 255, "g": 255, "b": 255, "intensity": 100, "duration": 0})
+        lights.start()
+        lights.join()
+
         print("recording...")
         self.frames = []
         while not self.stopped:
@@ -45,9 +52,25 @@ class Record(Thread):
         wave_file.setsampwidth(self.audio_interface.get_sample_size(FORMAT))
         wave_file.setframerate(RATE)
         wave_file.writeframes(b''.join(self.frames))
+
+        # check duration
+        frames = wave_file.getnframes()
+        rate = wave_file.getframerate()
+        duration = frames / float(rate)
+
         wave_file.close()
         print("finished recording")
 
+        if duration < 1:
+            self.tooShort = True
+            print("wave too short")
+
     def stop(self):
         self.stopped = True
-        return self.filename
+
+    def join(self):
+        Thread.join(self)
+        if not self.tooShort:
+            return self.filename
+        else:
+            return False
